5509e91ce35d1a9f213b0e0d6c6aa9fd645019f9
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2021-06-28 10:35:37 +0200

    adding correction so minus only is not considered as valid nb

--- srcs/libft/ft_isdigit_str.c
+++ srcs/libft/ft_isdigit_str.c
@@ -6,7 +6,7 @@
 /*   By: ccommiss <ccommiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/18 11:37:55 by ccommiss          #+#    #+#             */
-/*   Updated: 2021/06/22 12:53:02 by ccommiss         ###   ########.fr       */
+/*   Updated: 2021/06/28 10:30:19 by ccommiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	ft_isdigit_str(char *str)
 	i = 0;
 	while (str[i] != 0)
 	{
+		if (str[i] == '-' && ft_strlen(str) == 1)
+			return (0);
 		if ((!ft_isdigit(str[i]) && str[i] != '-')
 			|| (i != 0 && str[i] == '-'))
 			return (0);
